594eb45af68719d338a80f62332fb758c61c5440
Riquelme Milhomem <91623489+Rick-Farias@users.noreply.github.com>
2022-09-13 22:42:05 -0300

    Add files via upload

--- assets/script2.js
+++ assets/script2.js
@@ -44,9 +44,25 @@ let transacoesDados = [];
 const adicionar = document.getElementById("adicionar");
 adicionar.addEventListener('click', ()=>{    
     
-    dadosDB()
+    validador()
     
 })
+function validador(){
+    let nome = document.getElementById("nome").value;
+    let valor = document.getElementById("valor").value;
+
+    if(nome == "" && valor ==""){
+        alert("CAMPOS VAZIOS")
+    }else if(nome == "" && valor != ""){
+        alert("insira um nome")
+    }else if(valor == "" && nome != ""){
+        alert("insira um valor")
+    }else{
+        dadosDB()
+        valores()
+        recarregar()
+    }
+}
 function dadosDB(){
     let nome = document.getElementById("nome").value;
     let valor = parseInt(document.getElementById("valor").value)
@@ -59,7 +75,8 @@ function dadosDB(){
 
 // ADICIONAR DADOS AO FIRESTORE
 function adicionarDB(nome, valor, documentoId){
-    db.collection("TRANSACOES").doc().set({
+    db.collection("TRANSACOES").doc(documentoId).set({
+        id: documentoId,
         nome: nome,
         valor: valor
     }
@@ -74,6 +91,7 @@ function adicionarDB(nome, valor, documentoId){
             let dados = dataDados(data)
             AddTransacao(dados)
             InsertTransacoes(dados)
+            valores()
     })
 })
 
@@ -83,7 +101,9 @@ function AddTransacao(dados){
 function dataDados(data){
     let nome = data.nome;
     let valor = data.valor;
+    let id = data.id;
     let dados = {};
+    dados.id = id
     dados.nome = nome
     dados.valor = valor
     
@@ -91,20 +111,60 @@ function dataDados(data){
 }
 
 function InsertTransacoes(data){
-    
     let newli = document.createElement("li");
     let operador = data.valor < 0 ? '-' : '+'
     let valorCheio = Math.abs(data.valor)
-    
     let classCSS = data.valor < 0 ? 'desp' : 'rec';
 
     newli.classList.add(classCSS)
     newli.innerHTML += `
-        ${data.nome}<span>R$ ${operador}${valorCheio}</span><button class="btn-excluir" onclick="excluir()">x</button>
+        ${data.nome}<span>R$ ${operador}${valorCheio}</span><button class="btn-excluir" onclick="excluir(${data.id})"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z"/></svg></button>
     `
     listaTransacoes.append(newli)
+    
     }
 
-function excluir(){
-    console.log("teste")
+
+function excluir(id){
+let documentoId = String(id)
+    db.collection("TRANSACOES").doc(documentoId).delete()
+                .then(()=>{console.log("excluiu")})
+                .catch(err=>{console.log(err)})
+    
+recarregar()
 }
+
+
+function recarregar(){
+    setTimeout(()=>{
+        location.reload();
+    },450)
+}
+function valores(){
+        let valorTransacoes = transacoesDados.map(t => t.valor);
+        let valoresNegativos = valorTransacoes.filter(t => t < 0);
+        let valoresPositivos = valorTransacoes.filter(t => t > 0);
+        
+        let vp = 0;
+        for(i=0;i < valoresPositivos.length; i++){
+        
+            vp += parseInt(valoresPositivos[i]);
+        }
+
+        let vn = 0;
+        for(i=0; i < valoresNegativos.length; i++){
+            
+            vn += parseInt(valoresNegativos[i]);
+        }
+        console.log(vp , vn)
+        let vt = vp + vn;
+        InsertValores(vt, vp, vn)
+    }
+
+    function InsertValores(vt, vp, vn){
+        saldo.innerText = `R$ ${vt}`;
+        receitas.innerText = `R$  ${vp}`; 
+        despesas.innerText = `R$  ${vn}`;
+    }
+
+    
